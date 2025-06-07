8a6eb5600bc09296f444ea59f2d47098d3f447c9
jhong02 <justinhoang01@gmail.com>
2025-06-06 23:17:41 -0700

    added 123

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
   return (
     <div>
-      <h1>Welcome to the Barbershop Website test test test</h1>
+      <h1>Welcome to the Barbershop Website test test test 123</h1>
     </div>
   );
 }
